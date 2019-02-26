0a8f5c26967ee8f5ae88d57ea24da4fa57372ebe
Josue T <krothaloinf@gmail.com>
2019-02-25 21:44:07 -0600

    Initial commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="">
+    <link rel="stylesheet" href="/css/fixed.css">
+    <link rel="stylesheet" href="/css/lightbox.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Einar Exotics</title>
+    <link rel="shortcut icon" href="/img/logo2.png">
+    <link rel="stylesheet" href="/css/landstyle.css">
+    <title></title>
+  </head>
+  <body data-spy="scroll" data-target="#navbarResponsive">
+    <!--HOME SECTION -->
+    <div id="home" >
+      <nav class="navbar navbar-expand-md fied-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="img/logo2.png" alt=""/>Einar Exotics</a>
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"data-target="#navbarResponsive">
+                <span class="custom-toggler-icon"><i class="fas fa-bars"></i></span>
+          </button>
+          <div id="navbarResponsive" class="collapse navbar-collapse" >
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#portfolio">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#testimonials">Testimonials</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <div class="landing">
+      <div class="home-wrap">
+        <div class="home-inner" background="url('{{ asset('images/jungle3.jpg') }}')" >
+        </div>
+      </div>
+    </div>
+
+    </div>
+
+
+
+    <!-- END HOME SECTION -->
+    <!--ABOUT SECTION -->
+    <div id="about" class="offset">
+
+    </div>
+    <!-- END ABOUT SECTION -->
+    <!--PORTFOLIO SECTION -->
+    <div id="portfolio" class="offset">
+
+    </div>
+    <!-- END PORTFOLIO SECTION -->
+    <!--testimonials SECTION -->
+    <div id="testimonials" class="offset">
+
+    </div>
+    <!-- END testimonials SECTION -->
+<!-- Script source files-->
+<!--contact SECTION -->
+<div id="contact" class="offset">
+
+</div>
+<!-- END contact SECTION -->
+<!-- Script source files-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script type="js/custom.js">
+  </script>
+  <script type="js/lightbox.js">
+
+  </script>
+  <script type="js/validator.js">
+
+  </script>
+  <script type="js/contact.js">
+
+  </script>
+  <!--End of script source files -->
+  </body>
+</html>
